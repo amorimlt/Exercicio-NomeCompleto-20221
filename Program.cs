6795f70f3041165105594e99e66812674170e951
amorimlt <julianeamorim.lt@gmail.com>
2022-03-11 22:39:51 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome, sobrenome, sobrenomeMaisculo;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeMaisculo = sobrenome.ToUpper();
+
+Console.WriteLine($"\nNome Completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de Catalogo: {sobrenomeMaisculo}, {nome}");
